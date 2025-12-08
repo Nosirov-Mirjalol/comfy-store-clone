aab9437c4d682269b91e8473e2dc3d3a5c702394
Mirjalol <nosirovmirjalol869@gmail.com>
2025-12-08 18:29:17 +0500

    add loading

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,17 +1,24 @@
-import { Outlet } from "react-router-dom"
-import { Header,Navbar } from "../components/index"
+import { Outlet, useNavigation } from "react-router-dom";
+import { Header, Navbar } from "../components/index";
 
 const HomeLayout = () => {
+  const navigate = useNavigation();
+  const isLoading = navigate.state === "loading";
   return (
     <>
       <Header />
-      <Navbar/>
-      <div className="align-element">
-        <Outlet />
-      </div>
-      
+      <Navbar />
+      {isLoading ? (
+        <div className="h-screen flex items-center justify-center">
+          <span className="loading loading-ring loading-xl mb-10"></span>
+        </div>
+      ) : (
+        <div className="align-element">
+          <Outlet />
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomeLayout
+export default HomeLayout;
