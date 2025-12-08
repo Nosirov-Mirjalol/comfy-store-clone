a1c988dd1311d42d29f722b7fa7acf7bf16365c9
Mirjalol <nosirovmirjalol869@gmail.com>
2025-12-08 16:07:07 +0500

    add

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { Form, Link } from "react-router-dom"
+import FormInput from "./FormInput"
 
 const Filter = () => {
   return (
-    <div>
-      
-    </div>
+    <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      <FormInput label={"Search Product"} type={"text"} size={"input-sm"} />
+
+      {/* Buttons */}
+      <button type="submit" className="btn btn-primary btn-sm rounded-lg">SEARCH</button>
+      <Link to={"/products"} className="btn btn-secondary rounded-lg btn-sm">RESET</Link>
+    </Form>
   )
 }
 
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,8 +1,8 @@
-const FormInput = ({type,defaultValue,label}) => {
+const FormInput = ({type,defaultValue,label,size}) => {
   return (
     <fieldset className="fieldset py-3">
       <legend className="fieldset-legend">{label}</legend>
-      <input type={type} className="input input-primary" defaultValue={defaultValue} />
+      <input type={type} className={`input input-primary rounded-xl ${size}`} defaultValue={defaultValue} />
     </fieldset>
   );
 };
