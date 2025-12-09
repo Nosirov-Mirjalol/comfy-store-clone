65e4b245bf4d23c97d47c197432c5007d64c53c9
Mirjalol <nosirovmirjalol869@gmail.com>
2025-12-09 17:21:28 +0500

    add

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -6,7 +6,7 @@ import FormCheckbox from "./FormCheckbox"
 
 const Filter = () => {
   const {meta,params}=useLoaderData()
-	const { category, company, order, price, search, shipping } = params
+	const { category, company, order } = params
 
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
