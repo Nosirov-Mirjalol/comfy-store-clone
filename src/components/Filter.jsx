ee678df80f97915385fabc7ab237d0cf0029b455
Mirjalol <nosirovmirjalol869@gmail.com>
2025-12-09 16:39:44 +0500

    add

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -8,7 +8,7 @@ const Filter = () => {
   const {meta}=useLoaderData()
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-      <FormInput label={"Search Product"} type={"search"} size={"input-sm"} />
+      <FormInput name={"search"} label={"Search Product"} type={"search"} size={"input-sm"} />
       <FormSelect label="Select Category" name={"category"} size={"select-sm rounded-lg"} list={meta.categories}  />
       <FormSelect label="Select Company" name={"company"} size={"select-sm rounded-lg"} list={meta.companies}  />
       <FormSelect label="Sort by" name={"order"} size={"select-sm rounded-lg"} list={["a-z","z-a","high","low"]}  />
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,8 +1,8 @@
-const FormInput = ({type,defaultValue,label,size}) => {
+const FormInput = ({type,defaultValue,label,size,name}) => {
   return (
     <fieldset className="fieldset py-3">
       <legend className="fieldset-legend">{label}</legend>
-      <input type={type} className={`input  rounded-lg ${size}`} defaultValue={defaultValue} />
+      <input type={type} name={name} className={`input  rounded-lg ${size}`} defaultValue={defaultValue} />
     </fieldset>
   );
 };
