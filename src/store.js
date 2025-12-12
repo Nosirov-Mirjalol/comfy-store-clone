5c07417344325fc9aa794619a9a0a04b71fe833e
Mirjalol <nosirovmirjalol869@gmail.com>
2025-12-12 18:57:07 +0500

    add

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import cartReducer from './features/cart/cartSlice'
-import userReducer from './features/user/userSlice'
+import cartReducer from './Features/cart/cartSlice'
+import userReducer from './Features/user/userSlice'
+
 export const store = configureStore({
 	reducer: {
 		cartState:cartReducer,
